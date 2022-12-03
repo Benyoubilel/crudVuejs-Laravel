2e427d64ee48335c68c0fb422161b968bedccd91
Benyoubilel <bilel@ben-youssef.fr>
2022-12-03 20:53:11 +0100

    first commit

---
+++ src/components/modifier.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <!-- Button trigger modal -->
+
+
+    <!-- AddModal -->
+    <section class="container-fluid p-4" v-on:load="getusr()">
+        <div class="row dark">
+            <div class="col-md-12">
+                <div class="container-fluid p-4">
+                    <form class="form-floating" method="POST">
+
+                        <div class="modal-body">
+                            <label class="p-2">nom</label>
+                            <input type="text" class="form-control" name="nom" id="nom" placeholder="ali" v-model="nom">
+
+                            <label class="p-2">prenom</label>
+                            <input type="text" class="form-control" name="prenom" id="prenom" placeholder="benali"
+                                v-model="prenom" >
+                            <div class="modal-footer m-2">
+            
+                                <button type="submit" v-on:click="updateUsr(nom,prenom)" class="btn btn-success" id="submit">modifier</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+</template>
+<script>
+import axios from "axios";
+import useValidate from "@vuelidate/core";
+import { required,  minLength } from "@vuelidate/validators";
+console.log();
+export default {
+    name: 'modifier',
+    data() {
+        return {
+            user: [{
+                "nom": "",
+                "prenom": "",
+            }
+            ], 
+            id: this.$route.params.id,
+            v$: useValidate(),
+            nom: "",
+            prenom: "",
+            
+        };
+    },
+    validattion() {
+        return {
+            nom: { required, minLength: minLength(3) },
+            prenom: { required, minLength: minLength(3) },
+        };
+    },
+    async mounted() {
+        let res = await axios.get('http://127.0.0.1:8000/api/users');
+        this.user = res.data;
+    },
+    methods: {
+        async updateUsr( nom, prenom) {
+
+            //   let rep=  await axios.get('http://localhost:3000/user/' + id);
+            //   this.user= rep.data;
+             await axios.put('http://127.0.0.1:8000/api/users/' + this.id, { nom: nom, prenom: prenom });
+            window.location.href = "/"
+        },
+
+
+    },
+
+};
+</script>
